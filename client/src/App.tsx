4c27607297a627c0e2455d9b15a61cb038417ab7
ajuuz <ajuuz07@gmail.com>
2025-06-12 21:35:27 +0530

    lazy loading implemented

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,43 @@
-import { BrowserRouter as Router, Routes,Route } from 'react-router-dom'
-import {Toaster} from 'sonner'
-import UserRoutes from './routes/UserRoutes'
-import AdminRoutes from './routes/AdminRoutes'
-import MentorRoutes from './routes/MentorRoutes'
 import './App.css'
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom'
+import { Suspense,lazy } from 'react'
+import {Toaster} from 'sonner'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+
+function withDelay(importFunc:any, delay = 1500) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      importFunc().then(resolve);
+    }, delay);
+  });
+}
+
+// 👇 Lazy components with artificial delay
+const UserRoutes = lazy(() => withDelay(() => import('./routes/UserRoutes')) as any);
+const AdminRoutes = lazy(() => withDelay(() => import('./routes/AdminRoutes'))  as any);
+const MentorRoutes = lazy(() => withDelay(() => import('./routes/MentorRoutes'))  as any);
+
+
 const App = () => {
   return (
     <div >
       <Router>
       <Toaster position="bottom-center"  richColors toastOptions={{style: {flex:"flex",justifyContent:"center"}}}/> 
+      <Suspense fallback={  <div className="min-h-screen w-full pl-[10%] flex flex-col justify-center ">
+    <Skeleton 
+      count={8} 
+      height={70} 
+      
+      style={{ width: '90%', margin: 'auto auto', borderRadius: '10px' }}
+    />
+  </div>}>
         <Routes>
           <Route path='/*' element={<UserRoutes />} />
-          <Route path='/admin*' element={<AdminRoutes />} />
-          <Route path='/mentor*' element={<MentorRoutes />} />
+          <Route path='/admin/*' element={<AdminRoutes />} />
+          <Route path='/mentor/*' element={<MentorRoutes />} />
         </Routes>
+      </Suspense>
       </Router>
 
     </div>
