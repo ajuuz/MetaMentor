ae9b6f1a889fdcb23aa2d51328aadb1ec4da220c
ajuuz <ajuuz07@gmail.com>
2025-10-18 00:31:38 +0530

    added build script

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,7 +2,6 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import fs from 'fs';
 
 // https://vite.dev/config/
 export default defineConfig({
