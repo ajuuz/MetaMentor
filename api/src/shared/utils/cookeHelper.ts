49448788e40456f294f9815e98ec27f9efdefe88
ajuuz <ajuuz07@gmail.com>
2025-10-20 19:47:13 +0530

    cookie bug fixing

--- api/src/shared/utils/cookeHelper.ts
+++ api/src/shared/utils/cookeHelper.ts
@@ -1,19 +1,25 @@
 import { Response } from "express";
 import { config } from "shared/config";
 
-export const setCookie = (res: Response,accessToken: string,refreshToken: string) => {
+export const setCookie = (
+  res: Response,
+  accessToken: string,
+  refreshToken: string
+) => {
   const isProduction = config.node_env === "production";
+  console.log("isProduction",isProduction);
   res.cookie("accessToken", accessToken, {
     httpOnly: true,
     secure: isProduction,
+    sameSite: "none",
   });
 
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
     secure: isProduction,
+    sameSite: "none",
   });
 };
-
 
 export const setAccessCookie = (res: Response, accessToken: string) => {
   const isProduction = config.node_env === "production";
@@ -23,7 +29,7 @@ export const setAccessCookie = (res: Response, accessToken: string) => {
   });
 };
 
-export const clearCookies=(res:Response)=>{
-    res.clearCookie('accessToken')
-    res.clearCookie('refreshToken')
-}
+export const clearCookies = (res: Response) => {
+  res.clearCookie("accessToken");
+  res.clearCookie("refreshToken");
+};
